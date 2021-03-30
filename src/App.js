47e4745b2c47330d86c3f65b8f737c9cc4e755a6
Rinor Ermeni <rinor@bbros.eu>
2021-03-30 20:33:13 +0200

    Setup project using Webpack, Babel, React, React-dom

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>App Component</p>
+            </div>
+        );
+    }
+}
+export default App
